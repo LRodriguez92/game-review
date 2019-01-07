cbfb35fedb5729a9f105a21df8a41c77a9529664
student_123 <student_123@ool-ad020780.dyn.optonline.net>
2019-01-07 12:13:20 -0500

    added knock controller and config

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  vaildates :email, presence: true
   has_many :reviews
   has_many :comments
   has_many :ratings
+
+  def to_token_payload
+    {
+      sub: id,
+      email: email
+    }
+  end
 end
